392312e636e568947cf9710ad5f124acab2ec537
Marcin Galaszewski <poniatowski13@gmali.com>
2020-09-18 21:41:38 +0100

    Forgotten password - WIP

--- src/Controller/ForgottenPasswordController.php
+++ src/Controller/ForgottenPasswordController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validation;
 
 class ForgottenPasswordController extends AbstractController
 {
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
-     * @Route("/api/v1/forgotten_password", name="forgotten_password", methods={"GET"})
+     * @Route("/api/v1/user/forgotten_password", name="forgotten_password", methods={"GET"})
      */
-    public function forgottenPassword(Reguest $request): Response
+    public function forgottenPassword(Request $request): Response
     {
         $data = json_decode(
             $request->getContent(),
@@ -22,7 +32,7 @@ class ForgottenPasswordController extends AbstractController
 
         $validator = Validation::createValidator();
         $constraint = new Assert\Collection(array(
-            'email' => new Assert\Length(array('min' => 3, 'max' => 255)),
+            'email' => new Assert\Email(),
         ));
         $violations = $validator->validate($data, $constraint);
         if ($violations->count() > 0) {
@@ -33,8 +43,22 @@ class ForgottenPasswordController extends AbstractController
             return new JsonResponse(['error' => $errors], Response::HTTP_BAD_REQUEST);
         }
 
+        $user = $this->userRepository->findByEmailAddress($data['email']);
 
+        if ($user === null) {
+            return new JsonResponse(
+                [
+                    'error' => sprintf('The email address (%s) has not been recognised.', $data['email'])
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
 
-        return new JsonResponse('success', Response::HTTP_OK);
+        return new JsonResponse(
+            [
+                'success' => 'Email has been successfully sent to you email address'
+            ],
+            Response::HTTP_OK
+        );
     }
 }
