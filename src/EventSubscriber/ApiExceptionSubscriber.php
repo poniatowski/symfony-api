fbf8bb0bc80e2e553b172e2ec012fdebc482b98e
Marcin Galaszewski <poniatowski13@gmali.com>
2020-09-28 23:37:48 +0100

    Added skeleton of custom API handler exception.

---
+++ src/EventSubscriber/ApiExceptionSubscriber.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace App\EventSubscriber;
+
+use App\Exception\ApiExceptionInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class ApiExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if (!$exception instanceof ApiExceptionInterface) {
+            return;
+        }
+
+        $event->setResponse(new JsonResponse(
+            $exception->getMessage(),
+            $exception->getCode()
+        ));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
