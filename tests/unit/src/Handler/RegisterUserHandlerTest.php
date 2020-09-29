974470fa50ec3519e255541f2a09b1a725c4ba64
Marcin Galaszewski <poniatowski13@gmali.com>
2020-09-29 15:29:15 +0100

    Unit test

--- tests/unit/src/Handler/RegisterUserHandlerTest.php
+++ tests/unit/src/Handler/RegisterUserHandlerTest.php
@@ -1,23 +1,26 @@
 <?php namespace App\Tests\src\Handler;
 
+use App\Exception\ApiException;
 use App\Handler\RegisterUserHandler;
 use App\DTO\User as UserDTO;
 use App\Entity\User;
 use App\Tests\UnitTester;
 use App\Repository\UserRepository;
 use Codeception\Test\Unit;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterUserHandlerTest extends Unit
 {
     protected UnitTester $tester;
 
-    public function testCreateUserFromUserDto(): void
+    public function testCreateUserFromUserDtoOnSuccess(): void
     {
-        $userRepository  = $this->createMock(UserRepository::class);
-        $passwordEncoder = $this->createMock(UserPasswordEncoderInterface::class);
+        $userRepositoryMock  = $this->createMock(UserRepository::class);
+        $passwordEncoderMock = $this->createMock(UserPasswordEncoderInterface::class);
+        $loggerMock          = $this->createMock(LoggerInterface::class);
 
-        $passwordEncoder->expects($this->once())
+        $passwordEncoderMock->expects($this->once())
             ->method('encodePassword')
             ->willReturn('hashed_password');
 
@@ -27,16 +30,17 @@ class RegisterUserHandlerTest extends Unit
         $userDTO->password             = 'Password098';
         $userDTO->passwordConfirmation = 'Password098';
 
-        $userHandler = new RegisterUserHandler($userRepository, $passwordEncoder);
+        $userHandler = new RegisterUserHandler($userRepositoryMock, $passwordEncoderMock, $loggerMock);
         $userHandler->createUserFromUserDto($userDTO);
     }
 
-    public function testSaveUser(): void
+    public function testSaveUserOnSuccess(): void
     {
-        $userRepository  = $this->createMock(UserRepository::class);
-        $passwordEncoder = $this->createMock(UserPasswordEncoderInterface::class);
+        $userRepositoryMock  = $this->createMock(UserRepository::class);
+        $passwordEncoderMock = $this->createMock(UserPasswordEncoderInterface::class);
+        $loggerMock          = $this->createMock(LoggerInterface::class);
 
-        $passwordEncoder->expects($this->once())
+        $passwordEncoderMock->expects($this->once())
             ->method('encodePassword')
             ->willReturn('hashed_password');
 
@@ -44,7 +48,7 @@ class RegisterUserHandlerTest extends Unit
         $user->setPassword('hashed_password');
         $user->setEmail('user@domain.co.uk');
 
-        $userRepository->expects($this->once())
+        $userRepositoryMock->expects($this->once())
             ->method('saveUser')
             ->willReturn($user);
 
@@ -53,11 +57,36 @@ class RegisterUserHandlerTest extends Unit
         $userDTO->email                = 'user@domain.co.uk';
         $userDTO->password             = 'Password098';
 
-        $userHandler = new RegisterUserHandler($userRepository, $passwordEncoder);
+        $userHandler = new RegisterUserHandler($userRepositoryMock, $passwordEncoderMock, $loggerMock);
         $newUser = $userHandler->saveUser($userDTO);
 
         $this->assertSame('hashed_password', $newUser->getPassword());
         $this->assertSame('user@domain.co.uk', $newUser->getEmail());
         $this->assertSame(['ROLE_USER'], $newUser->getRoles());
+    }
+
+    public function testSaveUserOnException(): void
+    {
+        $userRepositoryMock  = $this->createMock(UserRepository::class);
+        $passwordEncoderMock = $this->createMock(UserPasswordEncoderInterface::class);
+        $loggerMock          = $this->createMock(LoggerInterface::class);
+
+        $passwordEncoderMock->expects($this->once())
+            ->method('encodePassword');
+
+        $user = new User();
+        $user->setPassword('hashed_password');
+        $user->setEmail('user@domain.co.uk');
+
+        $userDTO = new UserDTO();
+        $userDTO->email                = 'user@domain.co.uk';
+        $userDTO->password             = 'Password098';
+
+        $this->expectNotice();
+        $this->expectNoticeMessage('Unable to register user. Please, try again.');
+
+        $this->expectException(ApiException::class);
+        $userHandler = new RegisterUserHandler($userRepositoryMock, $passwordEncoderMock, $loggerMock);
+        $userHandler->saveUser($userDTO);
     }
 }
